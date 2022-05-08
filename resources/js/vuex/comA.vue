e4e54dee9c540564a7068f3eeeb463e070e22ab0
mohammed102578 <mohammed10257883@gmail.com>
2022-05-08 17:55:16 +0200

    mana

--- resources/js/vuex/comA.vue
+++ resources/js/vuex/comA.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <h1>
-            mana state of counter :{{$store.state.counter}}
+            mana good state of counter :{{$store.state.counter}}
         </h1>
     </div>
 </template>
